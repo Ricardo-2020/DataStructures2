f8d574d0961b37cd11d1a1ecf2e21dfb8a8222d5
Ricardo P <ricardopaucar1363@gmail.com>
2020-10-15 08:18:32 -0500

    created adding module

--- src/ArrayAdding.java
+++ src/ArrayAdding.java
@@ -1,3 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayAdding {
     
+    private ArrayList<Integer> arrayNumbers = new ArrayList<Integer>();
+    private int sum;
+
+    public void getNumbers(){
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter an integer: ");
+            int number = input.nextInt();
+            arrayNumbers.add(number);
+        }
+        input.close();
+    }
+
+    public void arraySum() {
+        sum = 0;
+        for (Integer userNum : arrayNumbers) {
+            sum = sum + userNum;
+        }
+        System.out.println("Sum: " + sum);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
+        ArrayAdding numArray = new ArrayAdding();
+        numArray.getNumbers();
+        numArray.arraySum();
     }
 }
